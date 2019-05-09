1b1886311cc20f3028b4bcc76592d9b640c5e017
Dominic Amato <dom@hologram.io>
2019-05-09 10:42:17 -0500

    Change naming convention for targets and packages

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "WiFiMobileConfig.swift",
+    name: "WiFiMobileConfig",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "WiFiMobileConfig.swift",
-            targets: ["WiFiMobileConfig.swift"]),
+            name: "WiFiMobileConfig",
+            targets: ["WiFiMobileConfig"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "WiFiMobileConfig.swift",
+            name: "WiFiMobileConfig",
             dependencies: []),
         .testTarget(
-            name: "WiFiMobileConfig.swiftTests",
-            dependencies: ["WiFiMobileConfig.swift"]),
+            name: "WiFiMobileConfigTests",
+            dependencies: ["WiFiMobileConfig"]),
     ]
 )
--- Tests/WiFiMobileConfig.swiftTests/MirrorDiffKit/Util.swift
+++
@@ -1,9 +0,0 @@
-func entries<K, V>(fromDictionary dictionary: [K: V]) -> [(key: K, value: V)] {
-    var result: [(key: K, value: V)] = []
-
-    for (key ,value) in dictionary {
-        result.append((key: key, value: value))
-    }
-
-    return result
-}
